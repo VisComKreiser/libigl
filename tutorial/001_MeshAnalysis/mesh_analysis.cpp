c0cf6ac846bffa0c7266a1a22b5b81768243c91b
Julian Kreiser <julian.kreiser@uni-ulm.de>
2020-10-06 13:31:02 +0200

    added new tutorials

---
+++ tutorial/001_MeshAnalysis/mesh_analysis.cpp
@@ -0,0 +1,184 @@
+#include <igl/readOFF.h>
+#include <igl/readOBJ.h>
+#include <igl/writeOBJ.h>
+#include <igl/readSTL.h>
+#include <igl/boundary_loop.h>
+#include <igl/map_vertices_to_circle.h>
+#include <igl/harmonic.h>
+#include <igl/collapse_edge.h>
+#include <igl/edge_flaps.h>
+#include <igl/decimate.h>
+#include <igl/shortest_edge_and_midpoint.h>
+#include <igl/parallel_for.h>
+#include <igl/opengl/glfw/Viewer.h>
+
+#include "tutorial_shared_path.h"
+
+#include <string>
+#include <filesystem>
+
+Eigen::MatrixXd V;
+Eigen::MatrixXi F;
+Eigen::MatrixXd V_uv;
+
+bool key_down(igl::opengl::glfw::Viewer& viewer, unsigned char key, int modifier)
+{
+    if (key == '1')
+    {
+      // Plot the 3D mesh
+        viewer.data().set_mesh(V, F);
+        viewer.core().align_camera_center(V, F);
+    }
+    else if (key == '2')
+    {
+      // Plot the mesh in 2D using the UV coordinates as vertex coordinates
+        viewer.data().set_mesh(V_uv, F);
+        viewer.core().align_camera_center(V_uv, F);
+    }
+
+    viewer.data().compute_normals();
+
+    return false;
+}
+
+void collapse_mesh(const Eigen::MatrixXd& V_in, const Eigen::MatrixXi& F_in, Eigen::MatrixXd& V_out, Eigen::MatrixXi& F_out, size_t target_size)
+{
+    using namespace Eigen;
+
+    // Prepare array-based edge data structures and priority queue
+    VectorXi EMAP;
+    MatrixXi E, EF, EI;
+    igl::min_heap< std::tuple<double, int, int> > Q;
+    VectorXi EQ;
+    // If an edge were collapsed, we'd collapse it to these points:
+    MatrixXd C;
+
+    // initialize
+    V_out = V_in;
+    F_out = F_in;
+
+    // calc. edge info
+    igl::edge_flaps(F_out, E, EMAP, EF, EI);
+    C.resize(E.rows(), V_out.cols());
+    VectorXd costs(E.rows());
+    // https://stackoverflow.com/questions/2852140/priority-queue-clear-method
+    // Q.clear();
+    Q = {};
+    EQ = VectorXi::Zero(E.rows());
+    {
+        VectorXd costs(E.rows());
+        igl::parallel_for(E.rows(), [&](const int e)
+            {
+                double cost = e;
+                RowVectorXd p(1, 3);
+                igl::shortest_edge_and_midpoint(e, V_out, F_out, E, EMAP, EF, EI, cost, p);
+                C.row(e) = p;
+                costs(e) = cost;
+            }, 10000);
+        for (int e = 0; e < E.rows(); e++)
+        {
+            Q.emplace(costs(e), e, 0);
+        }
+    }
+
+    // collapse edge
+    const auto max_iter = static_cast<size_t>(F.rows()) - target_size;
+    size_t num_collapsed = 0;
+    for (size_t idx = 0; idx < max_iter; idx++)
+    {
+        if (!igl::collapse_edge(igl::shortest_edge_and_midpoint, V_out, F_out, E, EMAP, EF, EI, Q, EQ, C))
+        {
+            break;
+        }
+        num_collapsed++;
+    }
+
+    std::cout << "num_collapsed: " << num_collapsed << std::endl;
+}
+
+int main(int argc, char* argv[])
+{
+    using namespace Eigen;
+    namespace fs = std::filesystem;
+
+    const std::string data_path{ "D:/tmp/decimated/1000_faces/" };
+
+    size_t too_many_boundaries{ 0 }, no_boundary{ 0 };
+    for (const auto& entry : fs::directory_iterator(data_path)) {
+        std::cout << entry.path().filename().string() << std::endl;
+
+        // load file
+        const auto& filename = entry.path().string();
+        if (!igl::readOBJ(filename, V, F)) {
+            std::cout << "could not read: " << filename << std::endl;
+            continue;
+        }
+
+        // get boundary loops
+        std::vector<std::vector<int>> b;
+        igl::boundary_loop(F, b);
+
+        // check if only one loop exists
+        if (b.size() > 1) {
+            std::cout << "too many boundaries: " << filename << std::endl;
+            too_many_boundaries++;
+            continue;
+        }
+        else if (b.empty()) {
+            std::cout << "no boundaries:       " << filename << std::endl;
+            no_boundary++;
+            continue;
+        }
+
+        // collapse to target size
+        //collapse_mesh(V, F, V, F, 1000);
+
+        // write to Eigen object
+        const auto& b_tmp = b[0];
+        const auto num_bnd_verts = b_tmp.size();
+        VectorXi bnd;
+        bnd.resize(num_bnd_verts, 1);
+        for (size_t i{ 0 }; i < num_bnd_verts; ++i)
+            bnd(i) = b_tmp[i];
+
+        // map boundary to circle
+        MatrixXd bnd_uv;
+        igl::map_vertices_to_circle(V, bnd, bnd_uv);
+
+        // flatten
+        igl::harmonic(V, F, bnd, bnd_uv, 1, V_uv);
+
+        // export
+        auto V_uv_padded = V_uv;
+        V_uv_padded.conservativeResize(V_uv_padded.rows(), V_uv_padded.cols() + 1);
+        MatrixXd padding_data(V_uv_padded.rows(), 1);
+        padding_data.setZero();
+        V_uv_padded.col(V_uv_padded.cols() - 1) = padding_data;
+        igl::writeOBJ(std::string("D:/tmp/decimated/1000_faces/flattened/") + entry.path().filename().string(), V_uv_padded, F);
+        /*
+        // Scale the uv
+        V_uv *= 5;
+
+        // Plot the mesh
+        igl::opengl::glfw::Viewer viewer;
+        viewer.data().set_mesh(V, F);
+        viewer.data().set_uv(V_uv);
+        viewer.callback_key_down = &key_down;
+
+        // Disable wireframe
+        viewer.data().show_lines = false;
+
+        // Draw checkerboard texture
+        viewer.data().show_texture = true;
+
+        // Launch the viewer
+        viewer.launch();
+        */
+    }
+
+    // draw statistics
+    std::cout << "meshes with too many open boundaries: " << too_many_boundaries << std::endl;
+    std::cout << "meshes with no open boundary:         " << no_boundary << std::endl;
+
+    return 0;
+}
---
+++ tutorial/002_ParameterCalculation/parameter_calculation.cpp
@@ -0,0 +1,144 @@
+#include <igl/readOFF.h>
+#include <igl/readOBJ.h>
+#include <igl/readSTL.h>
+#include <igl/PI.h>
+
+#include "tutorial_shared_path.h"
+
+#include <iostream>
+
+/*
+def triangleAreaCCW_2D(p0, p1, p2) :
+    # half the area of a planar parallelogram
+    # |v x w| = |v| * |w| * |sin(theta)| with theta = angle between v and w and v = vector(p0, p1) and w = vector(p0, p2)
+    # returned area is signed
+    v0 = p1 - p0
+    v1 = p2 - p0
+    # simplified cross product in 2D
+    return 0.5 * (v0[:, 0] * v1[:, 1] - v1[:, 0] * v0[:, 1])
+
+def triangleArea_3D(p0, p1, p2) :
+    #consider heron's formula with area of triangle if alle edge lengths are known
+    # S = sqrt(s * (s - a) * (s - b) * (s - c)) with s = 0.5 * (a + b + c) and a, b, c = edge lengths
+    v0 = p1 - p0
+    v1 = p2 - p0
+    return 0.5 * torch.cross(v0, v1).norm(dim = 1)
+
+def triangleAngles(p0, p1, p2, acos_clamp_value = 1.0 - 1e-5) :
+    v01 = F.normalize(p1 - p0)
+    v02 = F.normalize(p2 - p0)
+    v12 = F.normalize(p2 - p1)
+
+    # calculate angles at all vertices of the triangle using the cosine - similarity law
+    # pi minus the two first angles is the third one
+    phi0 = torch.acos(torch.clamp(tensorDot(v01, v02), -acos_clamp_value, acos_clamp_value)).view(-1, 1)
+    phi1 = torch.acos(torch.clamp(tensorDot(v01, v12), -acos_clamp_value, acos_clamp_value)).view(-1, 1)
+    phi2 = (math.pi - phi0 - phi1)
+
+return torch.cat([phi0, phi1, phi2], dim = 1)
+*/
+
+template <typename T>
+T clamp(T value, T min_value, T max_value)
+{
+    return std::min(std::max(value, min_value), max_value);
+}
+
+void triangleArea2D_CCW(const Eigen::MatrixXd& V, const Eigen::MatrixXi& F, Eigen::VectorXd& area)
+{
+    /*
+    # half the area of a planar parallelogram
+    # |v x w| = |v| * |w| * |sin(theta)| with theta = angle between v and w and v = vector(p0, p1) and w = vector(p0, p2)
+    # returned area is signed
+    v0 = p1 - p0
+    v1 = p2 - p0
+    # simplified cross product in 2D
+    */
+    area.resize(F.rows());
+
+    for (size_t idx{ 0 }; idx < area.size(); ++idx)
+    {
+        const Eigen::Vector2d& p0(V.row(F(idx, 0)));
+        const Eigen::Vector2d& p1(V.row(F(idx, 1)));
+        const Eigen::Vector2d& p2(V.row(F(idx, 2)));
+
+        const Eigen::Vector2d v0(p1 - p0);
+        const Eigen::Vector2d v1(p2 - p0);
+
+        area(idx) = 0.5 * (v0.x() * v1.y() - v1.x() * v0.y());
+    }
+}
+
+void triangleArea3D(const Eigen::MatrixXd& V, const Eigen::MatrixXi& F, Eigen::VectorXd& area)
+{
+    // returns the area for each triangle
+    area.resize(F.rows());
+
+    for (size_t idx{ 0 }; idx < area.size(); ++idx)
+    {
+        const Eigen::Vector3d& p0(V.row(F(idx, 0)));
+        const Eigen::Vector3d& p1(V.row(F(idx, 1)));
+        const Eigen::Vector3d& p2(V.row(F(idx, 2)));
+
+        const Eigen::Vector3d v0(p1 - p0);
+        const Eigen::Vector3d v1(p2 - p0);
+
+        area(idx) = 0.5 * v0.cross(v1).norm();
+    }
+}
+
+void triangleAngles(const Eigen::MatrixXd& V, const Eigen::MatrixXi& F, Eigen::MatrixXd& angles, const double acos_clamp = 1.0 - 1e-5)
+{
+    /*
+    # calculate angles at all vertices of the triangle using the cosine-similarity law
+    # pi minus the two first angles is the third one
+    */
+    angles.resize(F.rows(), F.cols());
+
+    for (size_t idx{ 0 }; idx < angles.rows(); ++idx)
+    {
+        const Eigen::Vector3d& p0(V.row(F(idx, 0)));
+        const Eigen::Vector3d& p1(V.row(F(idx, 1)));
+        const Eigen::Vector3d& p2(V.row(F(idx, 2)));
+
+        const Eigen::Vector3d v01 = (p1 - p0).normalized();
+        const Eigen::Vector3d v02 = (p2 - p0).normalized();
+        const Eigen::Vector3d v21 = (p1 - p2).normalized();
+
+        const double phi0 = std::acos(clamp(v01.dot(v02), -acos_clamp, acos_clamp));
+        const double phi1 = std::acos(clamp(v01.dot(v21), -acos_clamp, acos_clamp));
+
+        angles.row(idx) = Eigen::Vector3d(phi0, phi1, igl::PI - phi0 - phi1);
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    Eigen::MatrixXd V;
+    Eigen::MatrixXi F;
+
+    // Load a mesh
+    igl::readOBJ("D:/tmp/cube.obj", V, F);
+
+    // calculate triangle area
+    Eigen::VectorXd area;
+    triangleArea3D(V, F, area);
+    Eigen::MatrixXd angles;
+    triangleAngles(V, F, angles);
+
+    // debug print
+    std::cout << "area:" << std::endl;
+    for (size_t idx{ 0 }; idx < area.size(); ++idx)
+    {
+        std::cout << area(idx) << std::endl;
+    }
+    std::cout << std::endl;
+
+    std::cout << "angles:" << std::endl;
+    for (size_t idx{ 0 }; idx < angles.rows(); ++idx)
+    {
+        std::cout << angles.row(idx) << std::endl;
+    }
+
+    return 0;
+}
